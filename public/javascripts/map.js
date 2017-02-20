35754768d8b75a7dae55b9f20e4a22bf93520362
Michiel Barnaart <michielbarnaart@gmail.com>
2017-02-20 09:54:18 +0100

    disabled mapoptions

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -2,7 +2,9 @@ function initAutocomplete() {
       var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -33.8688, lng: 151.2195},
         zoom: 13,
-        mapTypeId: 'roadmap'
+        mapTypeId: 'roadmap',
+        mapTypeControl: false,
+        scrollwheel: false
 
       });
 
