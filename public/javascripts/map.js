61699de36071c9241b23bebb8dc86225d062c58f
Michiel Barnaart <michielbarnaart@gmail.com>
2017-02-20 09:20:51 +0100

    added map

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,27 +1,55 @@
-$(document).ready(function(){
-   const amsterdam = {
-     lat: 52.37020380000001,
-     lng: 4.89532650000001
-   };
+function initAutocomplete() {
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -33.8688, lng: 151.2195},
+        zoom: 13,
+        mapTypeId: 'roadmap'
 
- const map = new google.maps.Map(document.getElementById('map'), {
-     zoom: 15,
-     center: amsterdam
-   });
+      });
 
-   // Add restaurant markers to map
-   let markers = [];
+      // Create the search box and link it to the UI element.
+      var input = document.getElementById('pac-input');
+      var searchBox = new google.maps.places.SearchBox(input);
+      // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-   myPlaces.forEach(function(place){
-     let title = place.name;
-     let position = {
-       lat: place.location.coordinates[1],
-       lng: place.location.coordinates[0]
-     };
+      // Bias the SearchBox results towards current map's viewport.
+      map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+      });
 
-     var pin = new google.maps.Marker({ position, map, title  });
-     if(place.name === "Coffee") {pin.setIcon('https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png')}
-     else if (place.name === "books") {markers.push(pin)}
-   });
+      var markers = [];
+      // Listen for the event fired when the user selects a prediction and retrieve
+      // more details for that place.
+      searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
 
- });
+        if (places.length == 0) {
+          return;
+        }
+
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+          if (!place.geometry) {
+            console.log("Returned place contains no geometry");
+            return;
+          }
+          var icon = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+          };
+
+
+          if (place.geometry.viewport) {
+            // Only geocodes have viewport.
+            bounds.union(place.geometry.viewport);
+          } else {89
+            bounds.extend(place.geometry.location);
+          }
+        });
+        map.fitBounds(bounds);
+      });
+    }
